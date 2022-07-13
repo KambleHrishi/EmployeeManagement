b248b3f72e34d80d962b0e4c551919a7f8856e69
hrishikesh kamble <hrishikamble13@gmail.com>
2022-07-13 22:46:35 +0530

    UC3 added part time & wages

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
-﻿int IS_FULL_TIME = 1;
+﻿int IS_PART_TIME = 1;
+int IS_FULL_TIME = 2;
 int EMP_RATE_PER_HOUR = 20;
 int empHrs = 0;
 int empWage = 0;
 Random random = new Random();// new object creation
-int empCheck = random.Next(0, 2);//0 is inclusive,2 is exclusive: 0,1
+int empCheck = random.Next(0, 3);//0 is inclusive,3 is exclusive: 0,1,2
 if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
+}
+else if(empCheck == IS_PART_TIME)
+{
+    empHrs = 4;
 }
 else
 {
