f5c090ad46498afdc341509f16f698b7dc2120a0
hrishikesh kamble <hrishikamble13@gmail.com>
2022-07-13 23:32:01 +0530

    UC6 100 working hrs or max 20 days

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 const int IS_FULL_TIME = 2;
 const int EMP_RATE_PER_HOUR = 20;
 const int NUM_OF_WORKING_DAYS = 20;
+const int MAX_HRS_IN_MONTH = 100;
 int empHrs = 0;
 int empWage = 0;
 int totalEmpWage = 0;
-for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
-{
+int totalEmpHrs = 0;
+int totalWorkingDays = 0;
+while(totalEmpHrs<=MAX_HRS_IN_MONTH&&totalWorkingDays<NUM_OF_WORKING_DAYS)
+{   
+    totalWorkingDays++;
     Random random = new Random();
     int empCheck = random.Next(0, 3);
     switch (empCheck)
@@ -22,8 +26,8 @@ for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             break;
     }
 
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    totalEmpWage += empWage; //+= is a shorthand operator
-    Console.WriteLine("Emp Wage : " + empWage);
+    totalEmpHrs+=empHrs;
+    Console.WriteLine("Days: "+totalWorkingDays+"   " + "Emp Hrs: "+empHrs);
 }
+totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 Console.WriteLine("Total Emp Wage: "+totalEmpWage);
